de1761f1bd0fb9d1502d1a010615f80c87aa12ba
TeigLevingston <74582070+TeigLevingston@users.noreply.github.com>
2025-09-11 15:26:54 -0500

    Add files via upload
    Updated to take additional pictures of cats. Major refactoring after the initial beta.

--- pi/alarm.py
+++ pi/alarm.py
@@ -3,6 +3,7 @@ import cv2
 import time
 import numpy as np
 import datetime
+import random
 #import sys
 from gpiozero import DigitalOutputDevice
 
@@ -13,8 +14,10 @@ from gpiozero import Servo
  
 myGPIO=24
 myCorrection=0.5
-maxPW=(2.0+myCorrection)/1000
-minPW=(1.0-myCorrection)/1000
+#maxPW=(2.0+myCorrection)/1000
+#minPW=(1.0-myCorrection)/1000
+minPW=.0005
+maxPW=.005
 servo = Servo(myGPIO,min_pulse_width=minPW,max_pulse_width=maxPW)
 servo.min()
 #camera_index is the video device number of the camera 
@@ -25,24 +28,51 @@ model = YOLO("best.pt")
 cam = cv2.VideoCapture(camera_index)
 lastmean=0
 grayImage1 =0 # This always processes the second image
+newstart=1
 while True:
     ret, image1 = cam.read()
+    if newstart ==1:
+        newfilename = "images/" + datetime.datetime.now().isoformat() + ".jpg"
+        print(newfilename)
+        cv2.imwrite(newfilename,image1)
+        for x in range(3):
+            relay.on() # This will turn relay on
+            print(relay.is_active)
+            servo.max()
+            print("max")
+            time.sleep(.5)
+            relay.off() # This will not work :( but
+            print(relay.is_active)
+            servo.min()
+            print("min")
+            servo.max()
+
+
+            #servo.min()
+        newstart=0
     time.sleep(1)
+
     grayImage1= cv2.cvtColor(image1, cv2.COLOR_BGR2GRAY)
     grayImage2 = grayImage1
     motion = np.abs(np.mean(grayImage1) - lastmean)
     lastmean= np.mean(grayImage1)
+    ret, image0 = cam.read()
+    #if motion<1:
 
-    if motion>1:
+    if motion>3:
+        print("Motion:")
         print(motion)
         now = datetime.datetime.now().isoformat()
+        print("Start: "+datetime.datetime.now().isoformat())
         results = model(image1, stream=False)
+        print("end: " + datetime.datetime.now().isoformat())
         with open('summary.txt','a') as s:
             print(now +"\t",file=s, end="")
             #This generates a tab separated file for further analysis
             for conf in results:
                 Outlist = list()
                 count =  range(0,3)
+
                 for n in count:
                     pick = results[0].probs.top5[0]
                     pickname = conf.names[pick]
@@ -55,33 +85,66 @@ while True:
                 
                 print(pickname +"\t",file=s, end ="")
                 for thisitem in Outlist:
-                         print(thisitem +"\t",file=s,end="")
-            filename = "images/" + pickname + now + ".jpg"
+                    print(thisitem +"\t",file=s,end="")
+            filename = "images/likelycat"  + now + ".jpg"
+            filename0= "images/likelycat" + now + "_0.jpg"
+            filename1= "images/likelycat" + now + "_1.jpg"
             print(filename,file=s)
- 
-        with open('file2.txt', 'a') as f:
+
+
 
             for result in results:
 
                 nameidX = result.probs.top1
+                coonconf = results[0].probs.top5conf[2].item()
+                catconf = results[0].probs.top5conf[1].item()
                 names = result.names[nameidX]
                 confidence = result.probs.top1conf.cpu()
                 print(now)
-                cv2.imwrite(filename,image1)
-                if nameidX == 2 and confidence > .75:
-
-                    print("***ALARM***",file=f)                    
-                    print(filename, file=f)
-
-                    for x in range(20):
-                        relay.on() # This will turn relay on
-                        servo.max()
-                        print(relay.is_active)
-                        time.sleep(.2)
-                        relay.off() # turn the relay off
-                        servo.min()
-                        print(relay.is_active)
-                        time.sleep(.2)
+                print("Coon Confidence")
+                print(coonconf)
+                print(catconf)
+                #
+                if catconf> .15:
+                    cv2.imwrite(filename,image1)
+                    cv2.imwrite(filename0,image0)
+                    ret, image2 = cam.read()
+                    cv2.imwrite(filename1,image2)
+                    print("High Cat Confidence")
+                    print(coonconf)
 
 
-    
+                if nameidX == 2:
+                    if confidence > .75:
+                        filename = "images/raccoon"  + now + ".jpg"
+                        filename0= "images/raccoon" + now + "_0.jpg"
+                        filename1= "images/raccoon" + now + "_1.jpg"  
+                        with open('file2.txt', 'a') as f:
+                            print(now)
+                            print("***ALARM***",file=f)                    
+                            print(filename, file=f)
+                            cv2.imwrite(filename,image1)
+                            cv2.imwrite(filename0,image0)
+                            ret, image2 = cam.read()
+                            cv2.imwrite(filename1,image2)
+                            print("***ALARM***") 
+                            for x in range(20):
+                                pause=random.randrange(3,7)/10
+
+                                relay.on() # This will turn relay on
+                                servo.max()
+                                #print(relay.is_active)
+                                time.sleep(pause)
+                                relay.off() # turn the relay off
+                                servo.min()
+                                servo.max()
+                                
+                                #print(relay.is_active)
+                                time.sleep(pause)
+                                print(now)   
+
+
+
+
+
+
